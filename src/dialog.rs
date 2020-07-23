4745e2db8303add882adda0c669c9b188e9fcda0
mark-summerfield <mark@qtrac.eu>
2020-07-23 15:31:50 +0100

    with IM (but doesn't work since IM looks system-wide for IUP)

--- src/dialog.rs
+++ src/dialog.rs
@@ -3,11 +3,9 @@
 
 use crate::prelude::*;
 use crate::action::{on_ticker, on_timer, maybe_save, on_version, on_quit};
-use iup::{Ihandle, /*IM, */IUP};
-/*
+use iup::{Ihandle, IM, IUP};
 use std::env;
 use std::path::PathBuf;
-*/
 
 pub struct Dialog {
     pub dialog: *mut Ihandle,
@@ -61,11 +59,9 @@ impl Dialog {
         self.dialog = IUP.dialog(vbox);
         IUP.set_attribute(self.dialog, iup::NAME, &DIALOG);
         IUP.set_attribute(self.dialog, iup::TITLE, "Hello Rust IUP");
-        /*
         let exe = env::current_exe().unwrap_or(PathBuf::from("."));
         let image = exe.parent().unwrap_or(&exe).join("images/icon.png");
         let icon = IM.load_image(&image.as_path().to_string_lossy());
-        */
     }
 
     fn make_bindings(&mut self) {
